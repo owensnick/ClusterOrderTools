9d13053f4bfe27d7842228f6ec3a44e836f1193f
owensnick <owensnick@gmail.com>
2020-04-07 14:58:04 +0100

    Files generated by PkgTemplates

---
+++ src/ClusterOrderTools.jl
@@ -0,0 +1,5 @@
+module ClusterOrderTools
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ClusterOrderTools
+using Test
+
+@testset "ClusterOrderTools.jl" begin
+    # Write your own tests here.
+end
